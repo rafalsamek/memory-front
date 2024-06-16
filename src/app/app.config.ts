844b99021188977633220d075f3812a05687a12a
Sebastian Słomian <slomian@wszib.edu.pl>
2024-06-16 10:13:23 +0200

    Konfiguracja projektu

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,33 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideIcons, provideNgIconsConfig } from '@ng-icons/core';
+import {
+  heroArrowLeft,
+  heroArrowPath,
+  heroCheckCircle,
+  heroFaceFrown,
+  heroFaceSmile,
+  heroFolderOpen,
+  heroNoSymbol,
+  heroPlusCircle,
+  heroRocketLaunch,
+} from '@ng-icons/heroicons/outline';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideNgIconsConfig({ size: '1.5em' }),
+    provideIcons({
+      heroArrowLeft,
+      heroArrowPath,
+      heroNoSymbol,
+      heroPlusCircle,
+      heroCheckCircle,
+      heroFolderOpen,
+      heroRocketLaunch,
+      heroFaceFrown,
+      heroFaceSmile,
+    }),
+  ],
 };
