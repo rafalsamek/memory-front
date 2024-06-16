ed3f11ff0c3d89c725b26a3ebbc963b060589335
Sebastian Słomian <slomian@wszib.edu.pl>
2024-06-16 16:10:11 +0200

    Api Service

---
+++ src/app/api.service.ts
@@ -0,0 +1,33 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Category } from './models';
+
+const BASE_URL = 'http://localhost:8080';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  private readonly httpClient = inject(HttpClient);
+
+  getCategories(): Observable<Category[]> {
+    return this.httpClient.get<Category[]>(`${BASE_URL}/categories`);
+  }
+
+  getCategory(categoryId: number): Observable<Category> {
+    return this.httpClient.get<Category>(
+      `${BASE_URL}/categories/${categoryId}`
+    );
+  }
+
+  addCategory(categoryName: string): Observable<Category> {
+    return this.httpClient.post<Category>(`${BASE_URL}/categories`, {
+      name: categoryName,
+    });
+  }
+
+  removeCategory(categoryId: number): Observable<any> {
+    return this.httpClient.delete(`${BASE_URL}/categories/${categoryId}`);
+  }
+}
--- src/app/wordbank/categories/categories.component.ts
+++ src/app/wordbank/categories/categories.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { NgIcon } from '@ng-icons/core';
 import { NgForOf, NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { ApiService } from '../../api.service';
+import { Category } from '../../models';
 
 @Component({
   selector: 'app-categories',
@@ -11,18 +13,36 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.css',
 })
-export class CategoriesComponent {
-  categories: string[] = [];
+export class CategoriesComponent implements OnInit {
+  ngOnInit(): void {
+    this.fetchCategories();
+  }
+
+  private readonly apiService = inject(ApiService);
+
+  categoriesList: Category[] = [];
   showNewCategory = false;
   newCategory = '';
 
   addCategory() {
-    this.categories.push(this.newCategory);
+    this.apiService
+      .addCategory(this.newCategory)
+      .subscribe((category) => this.categoriesList.push(category));
     this.newCategory = '';
     this.showNewCategory = false;
   }
 
-  removeCategory(i: number) {
-    this.categories.splice(i, 1);
+  removeCategory(category: Category) {
+    this.apiService
+      .removeCategory(category.id)
+      .subscribe(() => this.fetchCategories());
+
+    // this.categoriesList.splice(i, 1);
+  }
+
+  fetchCategories() {
+    this.apiService
+      .getCategories()
+      .subscribe((categories) => (this.categoriesList = categories));
   }
 }
