f5f52ffe5f058300838b80fdcc497d4e29cc29c2
rsamek <rafal.samek@gmail.com>
2024-06-16 14:22:55 +0200

    categories

--- src/app/wordbank/categories/categories.component.ts
+++ src/app/wordbank/categories/categories.component.ts
@@ -2,14 +2,23 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { NgIcon } from '@ng-icons/core';
 import { NgForOf, NgIf } from '@angular/common';
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-categories',
   standalone: true,
-  imports: [RouterLink, NgIcon, NgForOf, NgIf],
+  imports: [RouterLink, NgIcon, NgForOf, NgIf, FormsModule],
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.css',
 })
 export class CategoriesComponent {
-  categories = [];
+  categories: string[] = [];
+  showNewCategory = false;
+  newCategory = '';
+
+  addCategory() {
+    this.categories.push(this.newCategory);
+    this.newCategory = '';
+    this.showNewCategory = false;
+  }
 }
