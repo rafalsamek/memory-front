2ea0ea53b253bf9991d0635a83caef6eeb12a008
Sebastian Słomian <slomian@wszib.edu.pl>
2024-06-16 11:05:49 +0200

    Header

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ['./src/**/*.{html,ts}'],
   theme: {
     extend: {},
+    container: {
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+      },
+    },
   },
   plugins: [],
 };
